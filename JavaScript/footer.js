3076a2d1b1fa27c03c7e90de01e36c701d7611cc
Derek Andersen <41839810+Dechrissen@users.noreply.github.com>
2021-02-09 10:34:42 -0500

    change `support` to `donate`, update footer (fa icons and version number)

--- JavaScript/footer.js
+++ JavaScript/footer.js
@@ -10,7 +10,12 @@ footer_list.setAttribute("style", "line-height: 75%;");
 var legal = document.createElement("p");
 var legalLink = document.createElement("a");
 legalLink.setAttribute("href", "/legal");
-var legalText = document.createTextNode("Legal");
+var legalText = document.createTextNode(" Legal");
+// begin font awesoe stuff
+var legalLogo = document.createElement("i");
+legalLogo.setAttribute("class", "fa fa-gavel");
+legalLink.appendChild(legalLogo);
+// end font awesome stuff
 legalLink.appendChild(legalText);
 legal.appendChild(legalLink);
 var legalLi = document.createElement("li");
@@ -21,16 +26,29 @@ var gh = document.createElement("p");
 var ghLink = document.createElement("a");
 ghLink.setAttribute("href", "https://github.com/Dechrissen/BanjoSpeedruns");
 ghLink.setAttribute("target", "_blank");
-var ghText = document.createTextNode("GitHub");
+var ghText = document.createTextNode(" GitHub");
+// begin font awesoe stuff
+var ghLogo = document.createElement("i");
+ghLogo.setAttribute("class", "fa fa-github");
+ghLink.appendChild(ghLogo);
+// end font awesome stuff
 ghLink.appendChild(ghText);
 gh.appendChild(ghLink);
 var ghLi = document.createElement("li");
 ghLi.appendChild(gh);
 
-// add links to footer_list
+// BanjoSpeedruns current version number
+var ver = document.createElement("p");
+var verText = document.createTextNode("BanjoSpeedruns v1.1");
+ver.appendChild(verText);
+var verLi = document.createElement("li");
+verLi.appendChild(ver);
 
+// add links to footer_list
 footer_list.appendChild(legalLi); // legal
 footer_list.appendChild(ghLi); // GitHub
+footer_list.appendChild(verLi); // version
+
 
 // add footer_list to footer
 footer.appendChild(footer_list);
